04a174d81e4ad7e973b6afb1792573f87e040dc2
NeoDim <Duffman7915492@gmail.com>
2020-09-09 09:41:04 +0300

    complete

---
+++ src/com/company/Author.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Author {
+
+    private String name;
+    private String email;
+    private char gender;
+
+    public Author(String name,String email,char gender){
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public char getGender(){
+        return gender;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void ToString(){
+        System.out.print(name + "("+gender+")"+" at "+ email);
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Author pushkin = new Author("Pushkin","lol@gmail.com",'m');
+	pushkin.setEmail("kolotushkin@gmail.com");
+	pushkin.ToString();
+    }
+}
